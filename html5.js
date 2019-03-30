96d578d8c539f3db0ae5ae6097b5fc5eda783aa6
calpis309 <penny@calpis309de-MacBook-Pro.local>
2019-03-30 19:19:30 +0800

    第一次上課內容

---
+++ html5.js
@@ -0,0 +1,42 @@
+$( document ).ready(function() {
+    console.log( "ready!" );
+    // document.addEventListener('click', changeColor);
+    $('#title').click(changeColor);
+});
+
+function changeColor() 
+{
+    alert( "changeColor!" );
+    $('#title').css("color", "black");
+}
+
+function varTest() 
+{
+    $("h1").css("color", "pink");
+
+    var arr = [1,2,3];
+    var int = 100;
+    var str = '10';
+    var boolen = false;
+
+    var obj = {
+        color: "red",
+        width: "100px"
+    };
+
+    for(var i=0; i<arr.length; i++)
+    {
+        var reasult = arr[i]+1;
+        arr[i] = reasult;
+        console.log(reasult);
+    }
+
+    var test = 5;
+    if (test<6) {
+        console.log("less");
+    } else if (test==6){
+        console.log("==");
+    } else {
+        console.log("more");
+    }
+}
